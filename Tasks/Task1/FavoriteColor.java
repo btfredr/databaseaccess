03ffb39b3a7a6599e78eaa5e0407f37657118693
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-09-08 10:53:00 +0200

    task init

---
+++ Tasks/Task1/FavoriteColor.java
@@ -0,0 +1,10 @@
+package Tasks.Task1;
+
+/*
+Create a JavaFX application that asks the user for their favorite color.
+The application should display a ComboBox with at least five predefined color options and allow the user to enter a custom color.
+ */
+
+public class FavoriteColor {
+    
+}
