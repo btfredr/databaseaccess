7a98bd4deb92b60bd1d56c35a85e350d3b85aaca
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-09-08 11:26:43 +0200

    added imports

--- Tasks/Task1/FavoriteColor.java
+++ Tasks/Task1/FavoriteColor.java
@@ -1,10 +1,15 @@
 package Tasks.Task1;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
+
 /*
 Create a JavaFX application that asks the user for their favorite color.
 The application should display a ComboBox with at least five predefined color options and allow the user to enter a custom color.
  */
 
-public class FavoriteColor {
+public class FavoriteColor extends Application {
     
 }
