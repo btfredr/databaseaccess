1d81839d8a280c5fc791b74dbcb9e52d760f9ce0
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-09-15 21:17:05 +0200

    added task desc

--- Tasks/Task9/src/main/java/VacationRental.java
+++ Tasks/Task9/src/main/java/VacationRental.java
@@ -5,6 +5,15 @@ import javafx.scene.control.*;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class VacationRental {
-    
+/*
+Develop an application for Lambert’s Vacation Rentals using JavaFX. 
+Include controls to select one of three locations, number of bedrooms, and an option to include meals. 
+Compute and display the total cost. Save as VacationRental.java.
+ */
+
+public class VacationRental extends Application {
+    @Override
+    public void start(Stage stage) {
+        // Location
+    }
 }
