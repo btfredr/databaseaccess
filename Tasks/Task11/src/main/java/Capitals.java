3760bedff54d271f81eba15a7e02299a1d7b2f00
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-09-17 12:57:45 +0200

    task init

---
+++ Tasks/Task11/src/main/java/Capitals.java
@@ -0,0 +1,17 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+/*
+    Write an application where the user selects a country from a list. 
+    Display the capital city of the selected country. 
+    Save as Capitals.java.
+ */
+
+public class Capitals {
+    
+}
