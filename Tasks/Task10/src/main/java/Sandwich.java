079c0df687ce56292896abec3af2287fabaf2c34
Fredrik Fordelsen <fredrikf2000@gmail.com>
2025-09-17 10:44:15 +0200

    task init

---
+++ Tasks/Task10/src/main/java/Sandwich.java
@@ -0,0 +1,9 @@
+/*
+    Design a JavaFX application for the Sublime Sandwich Shop. The
+    user selects a main ingredient and bread type from combo boxes or
+    list views. Display the calculated price. Save as Sandwich.java.
+ */
+
+public class Sandwich {
+    
+}
